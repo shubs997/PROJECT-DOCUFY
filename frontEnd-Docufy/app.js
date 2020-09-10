8d63162f25ddaf0399dbc947905328ee87dbef26
Shubham Jadhav <31961444+shubs997@users.noreply.github.com>
2020-09-10 11:42:07 +0530

    Add files via upload

---
+++ frontEnd-Docufy/app.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// function abc() {     // function expression closure to contain variables
+//   var i = 0;
+//   var pics = [ "assets/refresh.svg" ];
+//   var el = document.getElementById('icon');  // el doesn't change
+//   function toggle() {
+//       el.src = "assets/refresh.svg";           // set the image
+//       // i = (i + 1) % pics.length;  // update the counter
+//   }
+//   setInterval(toggle, 1); 
+// }; 
+
+// console.log("progress!!")
+
